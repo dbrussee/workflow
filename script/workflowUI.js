9cea43414ddf1f7d780be55295db782d4deedca4
BetterWayComputing <dbrussee@gmail.com>
2020-09-17 16:58:08 -0400

    Add step, Edit links

--- script/workflowUI.js
+++ script/workflowUI.js
@@ -1,4 +1,6 @@
 window.WorkflowUI = {
+    pickedSpot: null,
+    pickAction: null,
     arrowSize: {length:10, width:5},
     connectorLineCenterOffset: 24,
     pickedStep: null,
@@ -21,6 +23,18 @@ WorkflowUI.drawCanvas = function(workflow, div, callback) {
         WorkflowUI.drawCanvasLocation = div;
         div.innerHTML = "";
         var can = document.createElement("canvas");
+
+        // Sharpen up the resolution
+        //get DPI
+        var dpi = window.devicePixelRatio;
+        var style_height = getComputedStyle(can).getPropertyValue("height").slice(0, -2);
+        //get CSS width
+        let style_width = getComputedStyle(can).getPropertyValue("width").slice(0, -2);
+        //scale the canvas
+        can.setAttribute('height', style_height * dpi);
+        can.setAttribute('width', style_width * dpi);
+
+
         WorkflowUI.canvas = can;
         can.onmousedown = callback;
     
@@ -35,7 +49,7 @@ WorkflowUI.drawCanvas = function(workflow, div, callback) {
             var msg = "mouseup at " + event.clientX + ", " + event.clientY;
             //debug(msg);
         });
-        can.addEventListener("moustout", function(event) {
+        can.addEventListener("mouseout", function(event) {
             WorkflowUI.dragstart = null;
             WorkflowUI.canvas.style.cursor = "default";
         });
@@ -84,8 +98,8 @@ WorkflowUI.drawCanvas = function(workflow, div, callback) {
 
         });
     
-        can.width = 600;
-        can.height = 350;
+        can.width = 1200;
+        can.height = 1200;
         can.style.border = "1px dotted navy";
         div.appendChild(can);
     } else {
@@ -100,25 +114,6 @@ WorkflowUI.drawCanvas = function(workflow, div, callback) {
         var step = workflow.flow.steps[id];
         step.coords = null;
     }
-    /*
-    var columns = [[],[],[]]
-    for (var stepnum = 0; stepnum < workflow.flow.stepsList.length; stepnum++) {
-        var step = workflow.flow.steps[workflow.flow.stepsList[stepnum]];
-        var col = 0;
-        if (step.dependsOn.length > 0) {
-            for (var depnum = 0; depnum < step.dependsOn.length; depnum++){
-                var dstep = workflow.getStep(step.dependsOn[depnum]);
-                var dcol = dstep.location.col + 1; // add 1 for testing
-                if (dcol > col) col = dcol;
-            }
-            while (columns.length < col) {
-                columns.push([]);
-            }
-        };
-        step.location = {"col":col, "row":columns[col].length}
-        columns[col].push(step.id);
-    }
-    */
     for (var id in workflow.flow.steps) {
         var step = workflow.flow.steps[id];
         WorkflowUI.drawConnectors(WorkflowUI.canvas, step);
@@ -131,89 +126,7 @@ WorkflowUI.drawCanvas = function(workflow, div, callback) {
     }
 }
 
-WorkflowUI.draw = function(workflow, div, callback) {
-    WorkflowUI.masterFlow = workflow;
-    WorkflowUI.drawLocation = div;
-    WorkflowUI.drawCallback = callback;
-    WorkflowUI.redraw(workflow);
-}
-WorkflowUI.redraw = function() {
-    var workflow = WorkflowUI.masterFlow;
-    var tbl = document.createElement("table");
-    tbl.className = "workflow_table";
-    var tr = tbl.insertRow();
-    tr.className = "workflow_table_header";
-    var th = document.createElement("th");
-    th.colSpan = 8;
-    th.innerHTML = "Workflow: " + workflow.flow.name;
-    tr.appendChild(th);
-    tr = tbl.insertRow();
-    tr.className = "workflow_table_header";
-    th = document.createElement("th");
-    th.innerHTML = "ID"; tr.appendChild(th);
-    th = document.createElement("th");
-    th.innerHTML = "Title"; tr.appendChild(th);
-    th = document.createElement("th");
-    th.innerHTML = "Desc"; tr.appendChild(th);
-    th = document.createElement("th");
-    th.innerHTML = "Done"; tr.appendChild(th);
-    th = document.createElement("th");
-    th.innerHTML = "Status"; tr.appendChild(th);
-    th = document.createElement("th");
-    th.innerHTML = "Blocking"; tr.appendChild(th);
-    th = document.createElement("th");
-    th.innerHTML = "Blocked By"; tr.appendChild(th);
-    th = document.createElement("th");
-    th.innerHTML = "Comment"; tr.appendChild(th);
 
-    
-    for (var stepnum = 0; stepnum < workflow.flow.stepsList.length; stepnum++) {
-        var step = workflow.flow.steps[workflow.flow.stepsList[stepnum]];
-        tr = tbl.insertRow();
-        tr.dataset.workflowid = step.id;
-        tr.className = "workflow_table_datarow";
-        if (WorkflowUI.drawCallback != undefined && WorkflowUI.drawCallback != null) tr.onclick = WorkflowUI.drawCallback;
-        var td;
-        td = tr.insertCell(); td.innerHTML = step.id;
-        td = tr.insertCell(); td.innerHTML = step.title;
-        td = tr.insertCell(); td.innerHTML = step.desc;
-        var countBlocking = 0;
-        if (!step.completed) {
-            countBlocking = step.dependedOnBy.length;
-        }
-        var countBlockedBy = "";
-        for (var linknum = 0; linknum < step.dependsOn.length; linknum++) {
-            var otherStep = workflow.flow.steps[step.dependsOn[linknum]];
-            if (!otherStep.completed) {
-                countBlockedBy++;
-            }
-        }
-        var status = "";
-        if (countBlockedBy == 0) {
-            if (step.completed) {
-                tr.className += " done";
-                if (step.resolve_list.length == 0) {
-                    status = "Done";
-                } else {
-                    status = step.result;
-                }
-            } else {
-                status = "Ready";
-            }
-        } else {
-            status = "Blocked";
-        }
-        td = tr.insertCell(); td.innerHTML = (step.completed ? "Y":"N");
-        td = tr.insertCell(); td.innerHTML = status;
-        if (status == "Blocked") td.className = "blocked";
-        td = tr.insertCell(); td.innerHTML = (countBlocking == 0 ? "" : countBlocking == 1 ? "1 Step" : countBlocking + " Steps");
-        td = tr.insertCell(); td.innerHTML = (countBlockedBy == 0 ? "" : countBlockedBy == 1 ? "1 Step" : countBlockedBy + " Steps");
-        td = tr.insertCell(); td.innerHTML = step.comment;
-    }
-
-    WorkflowUI.drawLocation.innerHTML = "";
-    WorkflowUI.drawLocation.appendChild(tbl);
-}
 
 WorkflowUI.drawStep = function(can, step) {
     var wf = WorkflowUI.masterFlow;
@@ -224,6 +137,7 @@ WorkflowUI.drawStep = function(can, step) {
     var xy = WorkflowUI.getXY(step);
     var x = xy.x;
     var y = xy.y;
+    WorkflowUI.highlightStep(can, step, false);
     ctx.beginPath();
     ctx.moveTo(x, y+w); // Start at lower left
     ctx.lineTo(x+w, y+w); // Bottom
@@ -326,10 +240,17 @@ WorkflowUI.highlightStep = function(can, step, showHighlight) {
     var ctx = can.getContext("2d");
     var xy = WorkflowUI.getXY(step);
     ctx.beginPath();
-    ctx.moveTo(xy.x - 3, xy.y + xy.h + 10);
-    ctx.lineTo(xy.x + xy.w + 3, xy.y + xy.h + 10);
-    ctx.strokeStyle = (showHighlight ? "#00FFFF" : "#FFFFFF"); // aqua or white
-    ctx.lineWidth = 15;
+    ctx.strokeStyle = (showHighlight ? "yellow" : "white"); // aqua or white
+    ctx.lineWidth = (showHighlight ? 3 : 5);
+    //ctx.moveTo(xy.x - 3, xy.y + xy.h + 10);
+    //ctx.lineTo(xy.x + xy.w + 3, xy.y + xy.h + 10);
+    //ctx.strokeStyle = (showHighlight ? "#00FFFF" : "#FFFFFF"); // aqua or white
+    //ctx.lineWidth = 15;
+    ctx.moveTo(xy.x - 4, xy.y - 4);
+    ctx.lineTo(xy.x + xy.w + 4, xy.y - 4);
+    ctx.lineTo(xy.x + xy.w + 4, xy.y + xy.h + 4);
+    ctx.lineTo(xy.x - 4, xy.y + xy.h + 4);
+    ctx.closePath();
     ctx.stroke();
     ctx.restore();
     ctx.font = "8pt Arial";
@@ -337,6 +258,11 @@ WorkflowUI.highlightStep = function(can, step, showHighlight) {
     ctx.fillText(step.title, xy.x, xy.y+xy.w+12);
     ctx.restore();
     WorkflowUI.drawConnectors(can, step, true);
+}
+WorkflowUI.rowColForXY = function(x, y) {
+    var col = parseInt(x / (WorkflowUI.stepImageWidth + WorkflowUI.stepImageColumnGutter));
+    var row = parseInt(y / (WorkflowUI.stepImageWidth + WorkflowUI.stepImageVertSpace));
+    return {row:row, col:col};
 }
 WorkflowUI.getXY = function(step) {
     var w = WorkflowUI.stepImageWidth;
@@ -351,21 +277,16 @@ WorkflowUI.getXY = function(step) {
     return {x:x, y:y, w:w, h:w}
 }
 WorkflowUI.stepUnderXY = function(wf, x, y) {
-    var w = WorkflowUI.stepImageWidth;
+    var rslt = null;
+    var rc = WorkflowUI.rowColForXY(x, y);
     for (var id in wf.flow.steps) {
         var step = wf.flow.steps[id];
-        var xy = WorkflowUI.getXY(step);
-        if (x >= xy.x) {
-            if (x <= xy.x + w) {
-                if (y >= xy.y) {
-                    if (y <= xy.y + w) {
-                        return step;
-                    }
-                }
-            }
+        if (step.location.row == rc.row && step.location.col == rc.col) {
+            rslt = step;
+            break;
         }
     }
-    return null;
+    return rslt;
 }
 WorkflowUI.move = function(step, dir, num) {
     if (dir == "H") {
