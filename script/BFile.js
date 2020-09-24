5df67099d8ee53f175c48185630bbd3da674ce82
BetterWayComputing <dbrussee@gmail.com>
2020-09-24 15:34:25 -0400

    Update BFile.js

--- script/BFile.js
+++ script/BFile.js
@@ -3,14 +3,24 @@ B.File = function(container, onFileReady) {
     this.container = container;
     this.file = null;
     this.file_input = document.createElement("input");
+    this.file_input.id = "BFileButton";
     this.file_input.type = "file";
     this.file_input.accept = ".flow";
+    this.file_input.style.display = "none";
     this.callback = onFileReady;
     this.file_input.addEventListener('change', (event) => {
         this.file = event.target.files[0];
         this.callback(this.file);
     });
     container.appendChild(this.file_input);
+
+    //var lbl = document.createElement("label");
+    //lbl.htmlFor = "BFileButton";
+    var btn = document.createElement("button");
+    btn.innerHTML = "Open File...";
+    btn.onclick = function() {document.getElementById("BFileButton").click();}
+    //lbl.appendChild(btn);
+    container.appendChild(btn);
 
     this.reader = new FileReader();
     this.readerCallback = null;
